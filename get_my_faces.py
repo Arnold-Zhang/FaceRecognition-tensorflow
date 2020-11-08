6add2ed56e80166e5f789cd4500826aa77d90991
Arnold.Zhang <arnold.zxy@outlook.com>
2020-11-08 14:05:34 +0800

    format code

--- get_my_faces.py
+++ get_my_faces.py
@@ -1,7 +1,6 @@
 import cv2
 import dlib
 import os
-import sys
 import random
 
 output_dir = './my_faces'
@@ -10,21 +9,23 @@ size = 64
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
+
 # 改变图片的亮度与对比度
 def relight(img, light=1, bias=0):
     w = img.shape[1]
     h = img.shape[0]
     #image = []
-    for i in range(0,w):
-        for j in range(0,h):
+    for i in range(0, w):
+        for j in range(0, h):
             for c in range(3):
-                tmp = int(img[j,i,c]*light + bias)
+                tmp = int(img[j, i, c] * light + bias)
                 if tmp > 255:
                     tmp = 255
                 elif tmp < 0:
                     tmp = 0
-                img[j,i,c] = tmp
+                img[j, i, c] = tmp
     return img
+
 
 #使用dlib自带的frontal_face_detector作为我们的特征提取器
 detector = dlib.get_frontal_face_detector()
@@ -48,15 +49,16 @@ while True:
             x2 = d.left() if d.left() > 0 else 0
             y2 = d.right() if d.right() > 0 else 0
 
-            face = img[x1:y1,x2:y2]
+            face = img[x1:y1, x2:y2]
             # 调整图片的对比度与亮度， 对比度与亮度值都取随机数，这样能增加样本的多样性
-            face = relight(face, random.uniform(0.5, 1.5), random.randint(-50, 50))
+            face = relight(face, random.uniform(0.5, 1.5),
+                           random.randint(-50, 50))
 
-            face = cv2.resize(face, (size,size))
+            face = cv2.resize(face, (size, size))
 
             cv2.imshow('image', face)
 
-            cv2.imwrite(output_dir+'/'+str(index)+'.jpg', face)
+            cv2.imwrite(output_dir + '/' + str(index) + '.jpg', face)
 
             index += 1
         key = cv2.waitKey(30) & 0xff
